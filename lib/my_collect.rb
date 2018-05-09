6eb2f1ac0a95055856bb7b83f9704a54df57e718
Mary-Kate James <marykatj@gmail.com>
2018-05-09 17:48:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+def my_collect(collection)
+  if block_given?
+  i = 0
+  array = []
+  while i < collection.length
+    array.push(yield(collection[i]))
+    i += 1
+  end
+  end
+    array
+end
 
+#my_collect(collection) { |i| i.upcase }
+
+#my_collect(collection)
+  #collection.collect { |i| i.split(" ").first }
+
+#okay, that helps me narrow down the problem
+# somewhat?  im at work.....yeah Im sorry. gave my 2 weeks.thanks!
