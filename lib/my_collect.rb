a72b663e289fbafd0619487f1547caf87f80b756
Mary-Kate James <marykatj@gmail.com>
2018-05-09 17:53:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -10,11 +10,3 @@ def my_collect(collection)
   end
     array
 end
-
-#my_collect(collection) { |i| i.upcase }
-
-#my_collect(collection)
-  #collection.collect { |i| i.split(" ").first }
-
-#okay, that helps me narrow down the problem
-# somewhat?  im at work.....yeah Im sorry. gave my 2 weeks.thanks!
